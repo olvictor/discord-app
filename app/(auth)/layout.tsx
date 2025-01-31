c8568639af8f7a1b4828226403f05e4ba23d6d2c
Victor Oliveira <victorolvdev@gmail.com>
2025-01-31 11:44:00 -0300

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLoyut = ({children}: {children: React.ReactNode}) => {
+    return ( 
+        <div className="bg-red-500 h-screen">
+            {children}
+        </div>
+     );
+}
+ 
+export default AuthLoyut;
